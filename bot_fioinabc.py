5dce91ef184af780961ef90640f0fa4f9e70be22
Ramziya Makarova <ramziyaz@mail.ru>
2023-08-08 11:22:24 +0600

    Обновлены документы и код по чат боту

--- bot_fioinabc.py
+++ bot_fioinabc.py
@@ -5,7 +5,7 @@ from aiogram import Bot, Dispatcher, executor, types
 TOKEN = '6045201299:AAGgT6aR7avmn3BCCMmt8HV5N8aHu78S3SI'
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
-logging.basicConfig(filename='bot.log', level=logging.INFO)
+logging.basicConfig(filename='bot.log', level=logging.INFO,format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 def transliteration(fio_parts):
     translit_dict = {
@@ -13,7 +13,7 @@ def transliteration(fio_parts):
         "Ё": "E","ё": "e","Ж": "ZH","ж": "zh","З": "Z","з": "z","И": "I","и": "i","Й": "I","й": "i","К": "K","к": "k",
         "Л": "L","л": "l","М": "M","м": "m","Н": "N","н": "n","О": "O","о": "o","П": "P","п": "p","Р": "R","р": "r",
         "С": "S","с": "s","Т": "T","т": "t","У": "U","у": "u","Ф": "F","ф": "f","Х": "KH","х": "kh","Ц": "TS","ц": "ts",
-        "Ч": "CH","ч": "ch","Ш": "SH","ш": "sh","Щ": "SHCH","щ": "shch","Ы": "Y","ы": "y","Ъ": "IE","ъ": "ie","Э": "E",
+        "Ч": "CH","ч": "ch","Ш": "SH","ш": "sh","Щ": "SHCH","щ": "shch","Ы": "Y","ы": "y","Ъ": "IE","ъ": "ie", "Ь": "", "ь": "","Э": "E",
         "э": "e","Ю": "YU","ю": "yu","Я": "YA","я": "ya"}
 
     transliterated_parts = []
@@ -35,10 +35,14 @@ async def start(message: types.Message):
 
 @dp.message_handler()
 async def convert_fio(message: types.Message):
+    logging.info(f'Получено сообщение от пользователя: {message.text}')
+
     fio = message.text
     fio_parts = fio.split(' ')
     translit_fio = transliteration(fio_parts)
-    await message.reply(f"Транслитерированное ФИО: {translit_fio}")
+
+    logging.info(f'Ваше ФИО в международном формате: {translit_fio}')
+    await message.reply(f'Ваше ФИО в международном формате: {translit_fio}')
 
 if __name__ == '__main__':
     executor.start_polling(dp)
